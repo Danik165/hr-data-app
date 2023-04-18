226dc819682cbd57032132c7dd06d3e458d93005
Danik165 <30319587+Danik165@users.noreply.github.com>
2023-04-18 18:25:22 +0530

    Added ugly table

--- backend/app.py
+++ backend/app.py
@@ -2,40 +2,19 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-from src.secrets_of_my_heart import secret_key
-
-
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = secret_key  # Replace with a secure secret key
-jwt = JWTManager(app)
 db = SQLAlchemy(app)
 CORS(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     skill = db.Column(db.String(100), nullable=False)
     level = db.Column(db.String(100), nullable=False)
-    years = db.Column(db.String(100), nullable=False)
+    years = db.Column(db.String(100), nullable=False)  # Add the 'years' column
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
 
     def __repr__(self):
@@ -46,8 +25,9 @@ class Skill(db.Model):
             'id': self.id,
             'skill': self.skill,
             'level': self.level,
-            'years': self.years
+            'years': self.years  # Include 'years' field
         }
+
 
 
 class Employee(db.Model):
@@ -55,9 +35,7 @@ class Employee(db.Model):
     name = db.Column(db.String(100), nullable=False)
     activity = db.Column(db.String(100), nullable=False)
     skills = db.relationship('Skill', backref='employee', lazy=True, cascade="all, delete-orphan")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref='employee', uselist=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Added created_at field
 
     def __repr__(self):
         return f'<Employee {self.name}>'
@@ -69,6 +47,7 @@ class Employee(db.Model):
             'activity': self.activity,
             'created_at': self.created_at.isoformat(),
             'skills': [skill.to_dict() for skill in self.skills],
+
         }
 
 
@@ -96,10 +75,11 @@ def employee(employee_id):
     if request.method == 'PUT':
         employee.name = request.json['name']
         employee.activity = request.json['activity']
+        employee.experience = request.json['experience']
         employee.skills = []
         skills = request.json['skills']
         for skill_data in skills:
-            skill = Skill(skill=skill_data['skill'], level=skill_data['level'], years=skill_data['years'], employee=employee)
+            skill = Skill(skill=skill_data['skill'], level=skill_data['level'], employee=employee)
             db.session.add(skill)
 
         db.session.commit()
@@ -125,3 +105,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
