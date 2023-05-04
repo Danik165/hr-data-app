1695fa15c3bebc244d1c44578d58f2f45417847e
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-05-04 16:27:28 +0530

    added build scripts for react frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
-
+const path = require("path")
 const { requireAuth } = require("./middleware/authMiddleware")
 const authRoutes = require("./routes/authRoutes")
 const userRoutes = require("./routes/userRoutes/userRoutes")
@@ -15,14 +15,16 @@ app.use(cookieParser());
 app.use(authRoutes);
 app.use(userRoutes)
 
+app.use(express.static(path.join(__dirname,"..","frontend/build")))
+
 const PORT = process.env.PORT || 5000;
 
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello from your backend server.');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello from your backend server.');
+// });
 
 
 
