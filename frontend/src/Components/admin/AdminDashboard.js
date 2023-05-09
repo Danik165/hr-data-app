935df69a8a782e774c4e43cc6d1ea1ecf8c1864c
Danik165 <30319587+Danik165@users.noreply.github.com>
2023-05-09 21:15:12 +0530

    Login page looks better, added logout button to user profile, so need api from u. AdminDashboard.js is empty

--- frontend/src/Components/admin/AdminDashboard.js
+++ frontend/src/Components/admin/AdminDashboard.js
@@ -1,20 +1,46 @@
-// src/Components/AdminDashboard.js
 import React from 'react';
-
+import logo from '../../Images/logo.png';
+import './adminprofile.css';
 const AdminDashboard = ({ setIsAuthenticated }) => {
   const handleLogout = () => {
     localStorage.removeItem('access_token');
     setIsAuthenticated(false);
   };
+//
+//  return (
+//    <div>
+//      <h2>Admin Dashboard</h2>
+//      <p>Welcome to your dashboard.</p>
+//      <button onClick={handleLogout}>Logout</button>
+//    </div>
+//  );
+//};
+//
+//
+//export default (AdminDashboard);
+
+
 
   return (
-    <div>
-      <h2>Admin Dashboard</h2>
-      <p>Welcome to your dashboard.</p>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="dashboard-container">
+      <div className="Jeevan-logo">
+        <img src={logo} alt="Jeevan Logo" />
+      </div>
+      <div className="nav-header">
+        <button className="nav-button">Account</button>
+        <button className="nav-button" onClick={handleLogout}>Logout</button>
+      </div>
+      <div className="main-content">
+        <div className="card-container">
+          <div className="userprofile gradient-bg box">
+            <h2 className="userprofile-header">UserProfile</h2>
+            {/* User profile content will go here */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
+export default AdminDashboard;
 
-export default (AdminDashboard);
