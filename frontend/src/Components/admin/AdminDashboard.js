c2d55f0a6b3480fe78fc8089f1398a5aedb42064
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-05-11 17:01:42 +0530

    restructing

--- frontend/src/Components/admin/AdminDashboard.js
+++ frontend/src/Components/admin/AdminDashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from '../../Images/logo.png';
 import './adminprofile.css';
 import Header from '../header/header';
-// import Sidebar from '../sidebar/Sidebar';
-import AdminSidebar from './AdminSidebar';
+import AdminSidebar from './adminsidebar/AdminSidebar';
 import SkillTable from '../users/SkillTable';
-import AddEmployeeForm from './addEmployee';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import AddEmployeeForm from './addEmployee/addEmployee';
+import { Routes, Route } from 'react-router-dom';
 import Profile  from '../users/Profile';
 
 const AdminDashboard = ({ setIsAuthenticated }) => {
@@ -14,19 +12,16 @@ const AdminDashboard = ({ setIsAuthenticated }) => {
 
   return (
     <div className="dashboard-container">
-      
-
-
       <Header pageHeader="Admin Page" />
       <div  className='dashboard-body'>
-      <AdminSidebar setIsAuthenticated={setIsAuthenticated}  />
-      <Routes>
-        <Route path="/addusers" element={<AddEmployeeForm />} />
-        <Route path="/skills" element={<SkillTable />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+        <AdminSidebar setIsAuthenticated={setIsAuthenticated}  />
+        <Routes>
+          <Route path="/addusers" element={<AddEmployeeForm />} />
+          <Route path="/skills" element={<SkillTable />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </div>
-     
+    
     </div>
   );
 };
