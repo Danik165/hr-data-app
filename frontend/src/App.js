2b05a384f77a121ec0437ee2c649a37b1b4bd886
Danik165 <30319587+Danik165@users.noreply.github.com>
2023-05-12 16:42:58 +0530

    User and Admin Dashboard has sheet and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,20 @@ import Login from './Components/login/Login';
 //import Register from './Components/admin/Register';
 import UserDashboard from './Components/users/UserDashboard';
 import AdminDashboard from './Components/admin/AdminDashboard';
+import Profile from './Components/users/Profile';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import UserProtectedRoute from './Components/ProtectedRoute';
-import AddEmployee from './Components/admin/addEmployee'
+
 import './App.css'
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
   const [page, setPage] = useState('login');
-  
+
 
   return(
     <div className='App'>
-      
+
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={
@@ -24,7 +25,11 @@ const App = () => {
                                             </UserProtectedRoute>
                                           } />
           <Route exact path="/login" element={<Login setIsAuthenticated={setIsAuthenticated}/>} />
-          <Route path="/admin/*" element={
+          <Route path="/profile" element={
+                                    <UserProtectedRoute isAuthenticated={isAuthenticated}>
+                                      <Profile /> {/* Render Profile component here */}
+                                    </UserProtectedRoute>
+                                  } />          <Route path="/admin/*" element={
                                             <UserProtectedRoute isAuthenticated={isAuthenticated}>
                                               <AdminDashboard setIsAuthenticated={setIsAuthenticated}/>
                                             </UserProtectedRoute>
--- frontend/src/Components/users/Profile.js
+++ frontend/src/Components/users/Profile.js
@@ -1,11 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import './profile.css';
 
+const Profile = () => {
+  const [profile, setProfile] = useState({ email: '', phone: '' });
+  const [isEditing, setIsEditing] = useState(false);
 
-const Profile = () =>{
-    return(
-        <h1>
-            Profile
-        </h1>
-    )
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  const fetchProfile = async () => {
+
+    try {
+      const response = await fetch('http://localhost:5000/api/profile');
+      const data = await response.json();
+      setProfile(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const updateProfile = async (email, phone) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/profile', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, phone }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || 'Could not update profile.');
+      }
+
+      setProfile({ email, phone });
+      setIsEditing(false);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+if (isEditing) {
+  return (
+    <div className="profile-container">
+      <div className="profile-content">
+        <div className="profile-left">
+          <div className="profile-photo"></div>
+          <div className="profile-name">{profile.name}</div>
+          <div className="profile-role">{profile.role}</div>
+        </div>
+        <div className="profile-right">
+          <div className="profile-info">Information</div>
+          <div className="profile-email">Email: {profile.email}</div>
+          <div className="profile-phone">Phone: {profile.phone}</div>
+        </div>
+      </div>
+      <button onClick={() => setIsEditing(false)}>Back</button>
+    </div>
+  );
 }
+
+return (
+  <div className="profile-container">
+    <div className="profile-content">
+      <div className="profile-left">
+        <div className="profile-photo"></div>
+        <div className="profile-name">{profile.name}</div>
+        <div className="profile-role">{profile.role}</div>
+      </div>
+      <div className="profile-right">
+        <div className="profile-info">Information</div>
+        <div className="profile-email">Email: {profile.email}</div>
+        <div className="profile-phone">Phone: {profile.phone}</div>
+      </div>
+    </div>
+    <button onClick={() => setIsEditing(true)}>Edit</button>
+  </div>
+);
+
+};
 
 export default Profile;
