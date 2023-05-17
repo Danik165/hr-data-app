5a1150e8ecc9c7e1944bf24facd4ad24d1a3e39d
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-05-17 12:16:29 +0530

    added queries in seperate file.
    tested all admin apis

---
+++ backend/database/query.js
@@ -0,0 +1,12 @@
+
+const sqlQuery ={
+    selectUsers :"SELECT UserID as EmployeeId,Name,DepartmentName,RoleName from users inner join department on users.departmentID = department.departmentID inner join role on users.roleID = role.roleID",
+    selectUserById:"SELECT UserID,Name,EmailID,PhoneNumber,CurrentProject,DepartmentName,RoleName FROM company_skills.users inner join company_skills.department on users.departmentID = department.departmentID inner join company_skills.role on users.roleID = role.roleID where UserID = ?",
+    selectDepartments:"SELECT DepartmentName from department",
+    selectDepartmentIdByName:"Select DepartmentID from department where DepartmentName=?",
+    selectRoleIdByDepartmentIdandRoleName:"Select roleID from role where departmentID=? and RoleName = ?",
+    selectRoleNameByDepartmentId:"Select RoleName from role where DepartmentID =? ",
+    insertNewUser:"Insert into users(UserID,Name,EmailID,RoleID,DepartmentID,AccessID) values (?,?,?,?,?,?)",
+}
+
+module.exports.sqlQuery = sqlQuery;
