dcc00da9841547fef510b1dd9f1928808d5248e0
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-05-17 18:33:55 +0530

    added createotp functions

---
+++ backend/middleware/email/create_otp.js
@@ -0,0 +1,20 @@
+const generateUniqueId = require('generate-unique-id');
+
+ 
+// example 2
+
+const generateOtp = () =>{
+    const otp = generateUniqueId({
+      length: 6,
+      useLetters: false
+    });
+    
+    const id=generateUniqueId({
+        length:32
+    })
+    
+    return ({otp:otp,uniqueId:id})
+
+}
+
+module.exports.generateOtp = generateOtp;
