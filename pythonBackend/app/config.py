942f2c18c2324d57be59aa3ea67463c73c70c4ad
Danik165 <30319587+Danik165@users.noreply.github.com>
2023-05-17 11:33:25 +0530

    Removed key

--- pythonBackend/app/config.py
+++ pythonBackend/app/config.py
@@ -1,27 +1,27 @@
-# config.py
-import os
-class Config():
-# Replace this with your MySQL connection string
-    SQLALCHEMY_DATABASE_URI = "mysql://User:1234@localhost/company_skills"
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY='\xe23\xdf\x9f\x13\x98\x05|v\xcf3\xb81O\x95[\x86.K\xbc\xbaH\x1a\xca|\xf3k\xb2\xdbl\xae\xef'
-    JWT_SECRET_KEY='\xfa\x08\x81Y\xd7\xdc\x99\xfai\xfa\xb7\x81[\x0f\xc7\x89\x0ff.)\xe8\x87\xb8\xdc\x17D\x1en\x08\xc5 \xf7'
-
-
-
-    SECRET_KEY = os.urandom(32)
-    SECURITY_REGISTERABLE = True
-    SECURITY_PASSWORD_SALT = '184262654970072524547662832634214419157'
-    SECURITY_SEND_REGISTER_EMAIL = False
-    SECURITY_LOGIN_USER_TEMPLATE = 'login.html'
-    SECURITY_REGISTER_USER_TEMPLATE = 'register.html'
-    SECURITY_POST_LOGIN_VIEW = '/dashboard'
-    SECURITY_POST_LOGOUT_VIEW = '/'
-    SECURITY_POST_REGISTER_VIEW = '/dashboard'
-
-
-    # Use the extended forms we created
-    SECURITY_LOGIN_FORM = 'forms.ExtendedLoginForm'
-    SECURITY_REGISTER_FORM = 'forms.ExtendedRegisterForm'
+# # config.py
+# import os
+# class Config():
+# # Replace this with your MySQL connection string
+#     SQLALCHEMY_DATABASE_URI = "mysql://User:1234@localhost/company_skills"
+#
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#
+#     SECRET_KEY='\xe23\xdf\x9f\x13\x98\x05|v\xcf3\xb81O\x95[\x86.K\xbc\xbaH\x1a\xca|\xf3k\xb2\xdbl\xae\xef'
+#     JWT_SECRET_KEY='\xfa\x08\x81Y\xd7\xdc\x99\xfai\xfa\xb7\x81[\x0f\xc7\x89\x0ff.)\xe8\x87\xb8\xdc\x17D\x1en\x08\xc5 \xf7'
+#
+#
+#
+#     SECRET_KEY = os.urandom(32)
+#     SECURITY_REGISTERABLE = True
+#     SECURITY_PASSWORD_SALT = '184262654970072524547662832634214419157'
+#     SECURITY_SEND_REGISTER_EMAIL = False
+#     SECURITY_LOGIN_USER_TEMPLATE = 'login.html'
+#     SECURITY_REGISTER_USER_TEMPLATE = 'register.html'
+#     SECURITY_POST_LOGIN_VIEW = '/dashboard'
+#     SECURITY_POST_LOGOUT_VIEW = '/'
+#     SECURITY_POST_REGISTER_VIEW = '/dashboard'
+#
+#
+#     # Use the extended forms we created
+#     SECURITY_LOGIN_FORM = 'forms.ExtendedLoginForm'
+#     SECURITY_REGISTER_FORM = 'forms.ExtendedRegisterForm'
--- pythonBackend/secrets_of_my_heart.py
+++ pythonBackend/secrets_of_my_heart.py
@@ -1,5 +1,5 @@
-secret_key = '\xa8c\xb2\xe4\xa3\x01\xc2W"\xddO&\x11\x87n\t'
-# import secrets
-
-secret_salt = '184262654970072524547662832634214419157'
+# secret_key = '\xa8c\xb2\xe4\xa3\x01\xc2W"\xddO&\x11\x87n\t'
+# # import secrets
+#
+# secret_salt = '184262654970072524547662832634214419157'
 # secrets.SystemRandom().getrandbits(128)
