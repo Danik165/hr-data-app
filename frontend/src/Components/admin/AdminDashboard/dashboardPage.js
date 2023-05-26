5e2048cdc7915af349df913a879bdc74c9316138
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-05-26 15:55:21 +0530

    admin dashboard table and added a userprofile

---
+++ frontend/src/Components/admin/AdminDashboard/dashboardPage.js
@@ -0,0 +1,57 @@
+import { CDBInput } from "cdbreact";
+import { useEffect, useState } from "react";
+import DashboardTable from "./dashboardTable";
+
+const DashboardPage = () =>{
+    
+    const [searchValue,setSearchValue] = useState('')
+    const [profileList,setProfileList] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('')
+    
+    const getSearchResults = (e) =>{
+        if(e){
+
+            e.preventDefault();
+        }
+        fetch("http://localhost:5000/api/getdetails?" + new URLSearchParams({searchValue:searchValue}))
+        .then(response =>{
+            console.log(response.status)
+            if(response.status == 200){
+                response.json()
+                .then(data =>{
+                    //console.log(data);
+                    setErrorMessage('')
+                    setProfileList(data.data);
+
+                }
+                    )
+            }
+            else if(response.status == 400){
+                    setProfileList([])
+                    setErrorMessage("No Records Found")
+            }
+        })
+        .catch(err => {
+            console.log("Error Message")
+        })
+        console.log("Search Called")
+    }
+
+    useEffect(getSearchResults,[])
+    return(
+        <div class='dashboard-container w-100 h-100 d-flex'>
+            <div class='search-bar d-flex flex-row-reverse p-2'>
+               
+                <form onSubmit={e => getSearchResults(e)}>
+                <CDBInput placeholder="Search" type='text' icon="search" iconClass="text-muted" onChange={ e => setSearchValue(e.target.value)}/>
+                </form>
+            </div>
+            {errorMessage && <p>{errorMessage}</p>}
+            <div className="search-table">
+                <DashboardTable profileList={profileList}/>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardPage;
