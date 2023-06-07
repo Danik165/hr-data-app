dc0c95952e51c35e21fbe5b74b87130e8fdb5c09
Gowtham Ravichander <gowtham.ravichander@jeevantechnologies.com>
2023-06-07 10:49:46 +0530

    restructing done

--- frontend/src/Components/admin/userprofile/UserProfile.js
+++ frontend/src/Components/admin/userprofile/UserProfile.js
@@ -1,16 +1,14 @@
-import Profile from '../../users/Profile';
-import UserPage from '../../users/SkillTable';
+import Profile from '../../users/profile/Profile';
+import UserPage from '../../users/skilltable/SkillTable';
 
 const UserProfile = () =>{
     const queryParameters = new URLSearchParams(window.location.search)
     const id = queryParameters.get("id")
     return(
         <div class='user-profile-page-container h-100'>
-            {console.log(id)}
-        <Profile id={id} />
-        <h2>Skills</h2>
-        <UserPage id={id}/>
-        {/* <ProfileSkills id={id} /> */}
+            <Profile id={id} />
+            <h2>Skills</h2>
+            <UserPage id={id}/>
         </div>
     )
 }
